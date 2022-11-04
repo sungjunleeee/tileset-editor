b927070fcf1d09d3a9abe377e06c1ed578beb234
Sungjun <60735372+sungjunleeee@users.noreply.github.com>
2022-11-04 19:18:22 -0400

    drawing line based on the event

--- app.js
+++ app.js
@@ -3,15 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
-
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.fillStyle = "White";
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
+function onClick(event) {
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+canvas.addEventListener("click", onClick);
